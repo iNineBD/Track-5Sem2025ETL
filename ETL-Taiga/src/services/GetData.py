56662584fa188a205ec65415e15656dabbfc8c69
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-03-28 01:02:35 -0300

    feat: get data users and roles

--- ETL-Taiga/src/services/GetData.py
+++ ETL-Taiga/src/services/GetData.py
@@ -38,5 +38,30 @@ def pipeline_projets():
     projects = pd.DataFrame(projects)[campos]
     return projects
 
+def pipeline_users():
+    """pipeline para gerar dataframes de users"""
+    users = fetch_data("users")
+    campos = ['id','full_name_display','color']
+
+    list_roles = []
+    for user in users:
+        user_id = user.get("id")
+        roles = user.get("roles", [])
+
+        if isinstance(roles, list):
+            for role in roles:
+                if isinstance(role, dict):
+                    role_copy = role.copy()
+                    role_copy["user_id"] = user_id
+                    list_roles.append(role_copy)
+                else:
+                    list_roles.append({"user_id": user_id,"role_name": role})
+
+    users = pd.DataFrame(users)[campos]
+    roles = pd.DataFrame(list_roles) if list_roles else None
+
+    return users, roles
+
 # Executando a pipeline
 df_projects = pipeline_projets()
+df_users,df_roles = pipeline_users()
