613f7f1cb9f5942339c9ec7202852f356708f6b8
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-04-21 22:28:17 -0300

    rafactor: get status,tags and time

--- etl_taiga/src/services/get_data.py
+++ etl_taiga/src/services/get_data.py
@@ -48,16 +48,17 @@ def pipeline_projets():
     ids_projects = df_projects["id"].tolist()
     return df_projects, ids_projects
 
-_,ids_projects = pipeline_projets()
 
-
+# %%
 def pipeline_cards(id_projects):
     """
     Generate a DataFrame for cards.
     """
+    url_status = f"{TAIGA_HOST}/userstory-statuses"
     id_cards = []
     # coletar os ids das users stories por projeto
-    for (project_id) in id_projects:
+    url_cards = f"{TAIGA_HOST}/userstories?project="
+    for project_id in id_projects:
         response = requests.get(f"{url_cards}{project_id}", headers=headers, timeout=10)
         response.raise_for_status()
         cards = pd.DataFrame(response.json())
@@ -66,20 +67,87 @@ def pipeline_cards(id_projects):
     # unir os ids das users stories
     id_cards = list(np.concatenate(id_cards))
 
-    df_cards = pd.DataFrame(columns=["project", "assigned_to", "subject", "tags", "description", "created_date", "status"])
-
+    df_cards = pd.DataFrame(
+        columns=[
+            "id",
+            "assigned_to",
+            "subject",
+            "tags",
+            "description",
+            "created_date",
+            "status",
+        ]
+    )
+    url_cards_full = f"{TAIGA_HOST}/userstories/"
+    # coletar os dados filtrados das users stories
     for card_id in id_cards:
-        response = requests.get(f"{url_cards_full}{card_id}", headers=headers, timeout=10)
+        response = requests.get(
+            f"{url_cards_full}{card_id}", headers=headers, timeout=10
+        )
         response.raise_for_status()
-        card_data = response.json()
+        cards = response.json()
 
-        filtered_data = {campo: card_data.get(campo, None) for campo in df_cards.columns}
+        # filtrar os dados
+        filtered_data = {campo: cards.get(campo, None) for campo in df_cards.columns}
         df_temp = pd.DataFrame([filtered_data])
-
+        # adiciona os dados filtrados ao DataFrame se nao estiver vazio
         if not df_temp.empty:
             df_cards = pd.concat([df_cards, df_temp], ignore_index=True)
 
-    return df_cards
+    id_status = df_cards["status"].tolist()
+    df_status = pd.DataFrame(columns=["id", "name"])
+    # coletar os status das users stories
+    for status_id in id_status:
+        response = requests.get(
+            f"{url_status}/{status_id}", headers=headers, timeout=10
+        )
+        response.raise_for_status()
+        status = response.json()
+
+        # filtrar os dados
+        filtered_data = {campo: status.get(campo, None) for campo in df_status.columns}
+        df_temp = pd.DataFrame([filtered_data])
+        # adiciona os dados filtrados ao DataFrame se nao estiver vazio
+        if not df_temp.empty:
+            df_status = pd.concat([df_status, df_temp], ignore_index=True)
+
+    # coletrar as tags
+    df_tags = pd.DataFrame()
+    df_tags["name"] = df_cards["tags"].apply(lambda lista: [item[0] for item in lista])
+    df_tags["id"] = df_tags.index + 1
+
+    # id tag para df_cards
+    df_cards["id_tag"] = df_tags["id"]
+
+    df_cards = df_cards.rename(
+        columns={
+            "subject": "name",
+            "status": "fk_id_status",
+            "assigned_to": "fk_id_user",
+        }
+    ).drop(columns=["tags"])
+    df_cards["created_date"] = pd.to_datetime(df_cards["created_date"], utc=True)
+    df_time = pd.DataFrame()
+    df_time["id"] = range(1, len(df_cards) + 1)
+    df_time["year"] = df_cards["created_date"].dt.year
+    df_time["month"] = df_cards["created_date"].dt.month
+    df_time["day"] = df_cards["created_date"].dt.day
+    df_time["hour"] = df_cards["created_date"].dt.hour
+    df_time["minute"] = df_cards["created_date"].dt.minute
+    df_time = df_time[["id", "year", "month", "day", "hour", "minute"]]
+
+    # dropar coluna data
+    df_cards = df_cards.drop(columns=["created_date"])
+
+    id_users = df_cards["fk_id_user"].unique().tolist()
+
+    # limpar variaveis
+    del id_cards
+    del cards
+    del df_temp
+    gc.collect()
+
+    return df_cards, df_status, df_tags, id_users, df_time
 
 
 # %%
