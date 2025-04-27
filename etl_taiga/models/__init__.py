9b2d09502164b3cc2e7ce10e58c0898aa6d540be
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-04-27 20:53:05 -0300

    refactor: BaseModel for models declaration

--- etl_taiga/models/__init__.py
+++ etl_taiga/models/__init__.py
@@ -1,19 +1,11 @@
-import os
 from dotenv import load_dotenv
+import os
 from peewee import *
+from etl_taiga.db.Connection import database_config
 
 load_dotenv()
-
-TAIGA_USER = os.getenv("TAIGA_USER")
-TAIGA_PASSWORD = os.getenv("TAIGA_PASSWORD")
-TAIGA_HOST = os.getenv("TAIGA_HOST")
-TAIGA_PORT = os.getenv("TAIGA_PORT")
-TAIGA_DB = os.getenv("TAIGA_DB")
 DB_SCHEMA = os.getenv("DB_SCHEMA")
-
-db = PostgresqlDatabase(
-    TAIGA_DB, user=TAIGA_USER, password=TAIGA_PASSWORD, host=TAIGA_HOST, port=TAIGA_PORT
-)
+db = database_config()
 
 
 class BaseModel(Model):
