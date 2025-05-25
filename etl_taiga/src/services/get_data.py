b544f9907c6322900f7a7b4ec5eb2bca097e2f43
eduardofpaula <eduardo.paula01@fatec.sp.gov.br>
2025-05-24 21:46:50 -0300

    TRK-106 - feat: remove unused imports and clean up code in get_data.py

--- etl_taiga/src/services/get_data.py
+++ etl_taiga/src/services/get_data.py
@@ -4,20 +4,15 @@ Module for data extraction and transformation.
 
 import gc
 import os
-from unittest.mock import inplace
 import numpy as np
 import pandas as pd
 import requests
 from annotated_types.test_cases import cases
-from pandas.conftest import datetime64_dtype
-from pydantic.networks import email_validator
 from etl_taiga.src.services.auth import auth_taiga
 from dotenv import load_dotenv
 from prefect import task, flow
 from requests.auth import HTTPBasicAuth
 from datetime import datetime
-
-from etl_taiga.src.services.auth import auth_taiga
 
 load_dotenv()
 
